d96718988f6d9d89626c3dba40f68beb635b493f
VBacchelli <valebacchelli@gmail.com>
2024-11-24 00:35:28 +0100

    Base

---
+++ script.js
@@ -0,0 +1,31 @@
+const nextXmasDate = (currentTime) => {
+  let xmasDate = new Date(currentTime.getFullYear() + "-12-25T00:00:00");
+  if (currentTime.getTime() > xmasDate.getTime()) {
+    let nextYear = currentTime.getFullYear() + 1;
+    xmasDate = new Date(nextYear + "-12-25T00:00:00");
+  }
+  return xmasDate;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Unix timestamp (in seconds) to count down to
+  let nextXmas = nextXmasDate(new Date()).getTime() / 1000;
+
+  // Set up FlipDown
+  let flipdown = new FlipDown(nextXmas)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log("The countdown has ended!");
+    });
+});
+
+setTimeout(() => {
+    const message = document.querySelector('.message');
+    if (message) {
+        message.style.display = 'none';
+    }
+}, 5000); // Matches the animation duration
