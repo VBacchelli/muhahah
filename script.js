ed40a7b3b5fb33d2c9d01ebe091c451e3fc7c8ce
Vale <valebacchelli@gmail.com>
2024-11-28 20:45:44 +0100

    flipdown

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const nextXmasDate = (currentTime) => {
-  let xmasDate = new Date(currentTime.getFullYear() + "-12-25T00:00:00");
+const decFirstDate = (currentTime) => {
+  let xmasDate = new Date(currentTime.getFullYear() + "-12-01T00:00:00");
   if (currentTime.getTime() > xmasDate.getTime()) {
     let nextYear = currentTime.getFullYear() + 1;
-    xmasDate = new Date(nextYear + "-12-25T00:00:00");
+    xmasDate = new Date(nextYear + "-12-01T00:00:00");
   }
   return xmasDate;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   // Unix timestamp (in seconds) to count down to
-  let nextXmas = nextXmasDate(new Date()).getTime() / 1000;
+  let deadline = decFirstDate(new Date()).getTime() / 1000;
 
   // Set up FlipDown
-  let flipdown = new FlipDown(nextXmas)
+  let flipdown = new FlipDown(deadline)
 
     // Start the countdown
     .start()
